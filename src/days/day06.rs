521e650c3f2cb30296a62d789f0b74759379712b
ryanshappell <hoovcat99@gmail.com>
2020-12-09 22:47:20 -0800

    Day 6 pt2 solution

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,7 +1,7 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 pub fn run() {
-    let file = fs::read_to_string("inputs/test06.txt").expect("Failed to read file");
+    let file = fs::read_to_string("inputs/day06.txt").expect("Failed to read file");
 
     // Part 1
     let mut count = 0;
@@ -22,5 +22,16 @@ pub fn run() {
     println!("{}", count);
 
     // Part 2
-    
+    count = 0;
+    let mut curr_group: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+    for line in file.lines() {
+        if line == "" {
+            count += curr_group.len();
+            curr_group = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+        } else {
+            curr_group = &curr_group & &line.chars().collect();
+        }
+    }
+    count += curr_group.len();
+    println!("{}", count);
 }
